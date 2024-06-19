82b23b2c87220b99cbfd299c90e08935422ab624
HeisenBerg? <admin@tiktokastudio.com>
2024-06-19 13:30:56 +0800

    Update setup.ps1
    change exe command to msi insttall  https://www.advancedinstaller.com/install-msi-files-with-powershell.html

--- setup.ps1
+++ setup.ps1
@@ -1,3 +1,3 @@
 Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
-& {$P = $env:TEMP + '\chromeremotedesktophost.msi'; Invoke-WebRequest 'https://dl.google.com/dl/edgedl/chrome-remote-desktop/chromeremotedesktophost.msi' -OutFile $P; Start-Process $P -Wait; Remove-Item $P}
-& {$P = $env:TEMP + '\chrome_installer.msi'; Invoke-WebRequest 'https://dl.google.com/tag/s/dl/chrome/install/googlechromestandaloneenterprise64.msi' -OutFile $P; Start-Process -FilePath $P -Args '/install' -Verb RunAs -Wait; Remove-Item $P}
+& {$P = $env:TEMP + '\chromeremotedesktophost.msi'; Invoke-WebRequest 'https://dl.google.com/dl/edgedl/chrome-remote-desktop/chromeremotedesktophost.msi' -OutFile $P;Start-Process 'msiexec.exe' -ArgumentList '/I $P' -Wait; Remove-Item $P}
+& {$P = $env:TEMP + '\chrome_installer.msi'; Invoke-WebRequest 'https://dl.google.com/tag/s/dl/chrome/install/googlechromestandaloneenterprise64.msi' -OutFile $P; Start-Process 'msiexec.exe' -ArgumentList '/I $P ' -Wait; Remove-Item $P}
