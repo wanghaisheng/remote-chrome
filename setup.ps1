a503fedbff4a37f181c216a0d4cb0dde0b9c2231
HeisenBerg? <admin@tiktokastudio.com>
2024-06-19 13:54:05 +0800

    Update setup.ps1
    https://gist.github.com/rysstad/7c765f840156a68ca30a

--- setup.ps1
+++ setup.ps1
@@ -1,3 +1,6 @@
 Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
-& {$P = $env:TEMP + '\chromeremotedesktophost.msi'; Invoke-WebRequest 'https://dl.google.com/dl/edgedl/chrome-remote-desktop/chromeremotedesktophost.msi' -OutFile $P;Start-Process 'msiexec.exe' -ArgumentList '/I $P' -Wait; Remove-Item $P}
-& {$P = $env:TEMP + '\chrome_installer.msi'; Invoke-WebRequest 'https://dl.google.com/tag/s/dl/chrome/install/googlechromestandaloneenterprise64.msi' -OutFile $P; Start-Process 'msiexec.exe' -ArgumentList '/I $P ' -Wait; Remove-Item $P}
+$tmpFile = [System.IO.Path]::GetTempFileName()
+
+& {$P = $env:TEMP + '\chromeremotedesktophost.msi'; Invoke-WebRequest 'https://dl.google.com/dl/edgedl/chrome-remote-desktop/chromeremotedesktophost.msi' -OutFile $P;Start-Process 'msiexec.exe' -ArgumentList '/I $P  /qn /L*v $tmpFile'  -NoNewWindow -Wait; Remove-Item $P}
+
+& {$P = $env:TEMP + '\chrome_installer.msi'; Invoke-WebRequest 'https://dl.google.com/tag/s/dl/chrome/install/googlechromestandaloneenterprise64.msi' -OutFile $P; Start-Process 'msiexec.exe' -ArgumentList '/I $P  /qn /L*v $tmpFile'  -NoNewWindow  -Wait; Remove-Item $P}
